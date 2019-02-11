f77a4d41c8dcfb124330eadd5870ff7de4fca036
Jenny Chu <jchudesign0930@gmail.com>
2019-02-11 01:01:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,4 @@
 def oxford_comma(array)
-array = ["kiwi"]
-array.join
-string = ["kiwi","durian"]
-string.join (" and ")
-end
-
-def oxford_comma(array)
   if array.length == 1
     return "#{array[0]}"
   elsif array.length == 2
