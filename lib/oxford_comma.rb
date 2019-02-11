356656cc3ea179e22d8f027327071b28d81884b7
Jenny Chu <jchudesign0930@gmail.com>
2019-02-11 00:58:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,3 +5,14 @@ string = ["kiwi","durian"]
 string.join (" and ")
 end
 
+def oxford_comma(array)
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
+  end
+end
+
